9699e7e5c7418994d768138d0ffc4c8d38315a6c
Eelco Los <5102501+EelcoLos@users.noreply.github.com>
2024-07-09 08:37:28 +0200

    c#11 code style changes

--- TestErpApp/Controllers/KeyController.cs
+++ TestErpApp/Controllers/KeyController.cs
@@ -1,35 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace TestErpApp.Controllers;
 
-namespace TestErpApp.Controllers
+[ApiController]
+[Route("[controller]")]
+public class KeyController(IServiceProvider services) : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class KeyController(IServiceProvider services) : ControllerBase
+    private readonly IServiceProvider _services = services;
+
+    [HttpGet]
+    public KeyModel Get()
     {
-        private readonly IServiceProvider _services = services;
-
-        [HttpGet]
-        public KeyModel Get()
+        var store = _services.GetService<KeyStore>();
+        return new KeyModel
         {
-            var store = _services.GetService<KeyStore>();
-            return new KeyModel
-            {
-                Key = store.Key
-            };
-        }
-
-        [HttpPost]
-        public ActionResult Set([FromBody] KeyModel key)
-        {
-            var store = _services.GetService<KeyStore>();
-            store.Key = key.Key;
-
-            return new OkResult();
-        }
+            Key = store?.Key ?? string.Empty
+        };
     }
 
-    public class KeyModel
+    [HttpPost]
+    public ActionResult Set([FromBody] KeyModel key)
     {
-        public string Key { get; set; }
+        var store = _services.GetService<KeyStore>();
+        store!.Key = key.Key;
+
+        return new OkResult();
     }
+}
+
+public class KeyModel
+{
+    public required string Key { get; set; }
 }
--- TestErpApp/Pages/Error.cshtml.cs
+++ TestErpApp/Pages/Error.cshtml.cs
@@ -1,21 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Diagnostics;
+namespace TestErpApp.Pages;
 
-namespace TestErpApp.Pages
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
+    private readonly ILogger<ErrorModel> _logger = logger;
+
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    public void OnGet()
     {
-        private readonly ILogger<ErrorModel> _logger = logger;
-
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
 }
