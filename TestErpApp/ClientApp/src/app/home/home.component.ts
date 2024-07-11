d6b5e2f7d7004b496aa225d8c8354fde799e6673
Eelco Los <5102501+EelcoLos@users.noreply.github.com>
2024-07-11 09:43:28 +0200

    remove track function

--- TestErpApp/ClientApp/src/app/home/home.component.ts
+++ TestErpApp/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { interval } from 'rxjs';
 import { switchMap, catchError } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UpdateStatusDialogComponent } from "../update-status-dialog/update-status-dialog.component";
+import { UpdateStatusDialogComponent } from '../update-status-dialog/update-status-dialog.component';
 import { NotificationModel } from '../models/notification';
 import { ClientService } from '../services/client.service';
 import { ProcessedStatus } from '../models/processedstatus';
@@ -11,7 +17,12 @@ import { ProcessedStatus } from '../models/processedstatus';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, UpdateStatusDialogComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    UpdateStatusDialogComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,9 +33,17 @@ export class HomeComponent {
   notifications = signal<NotificationModel[]>([]);
   public file = signal<string>('');
   showDialog = signal(false);
-  fileIdFromNotification = computed(() => this.notifications().map(n => n.fileId));
-  applicationName = computed(() => this.notifications().map(n => n.applicationName));
-  version = computed(() => this.notifications().map(n => n.fileVersion || "2022-02-16T11:12:56.3052287Z"));
+  fileIdFromNotification = computed(() =>
+    this.notifications().map((n) => n.fileId),
+  );
+  applicationName = computed(() =>
+    this.notifications().map((n) => n.applicationName),
+  );
+  version = computed(() =>
+    this.notifications().map(
+      (n) => n.fileVersion || '2022-02-16T11:12:56.3052287Z',
+    ),
+  );
   public askJson: boolean = false;
   public isJson: boolean = false;
   public key: string = '';
@@ -32,33 +51,29 @@ export class HomeComponent {
   constructor() {
     interval(2000)
       .pipe(
-        switchMap(() =>
-          this.clientService.getNotifications()
-        ),
+        switchMap(() => this.clientService.getNotifications()),
         catchError((error) => (this.file = error)),
       )
-      .subscribe((result) => (this.notifications.set(result as NotificationModel[])));
+      .subscribe((result) =>
+        this.notifications.set(result as NotificationModel[]),
+      );
     this.clientService.getKey().subscribe((result) => (this.key = result.key));
   }
 
   public GetFile(notification: NotificationModel) {
-
-    return this.clientService.GetFile(notification, this.key, this.askJson)
-      .subscribe(
-        (result) =>
-        (this.file.set(this.IsJson(result)
-          ? JSON.parse(result)
-          : result.toString())
-        ));
+    return this.clientService
+      .GetFile(notification, this.key, this.askJson)
+      .subscribe((result) =>
+        this.file.set(
+          this.IsJson(result) ? JSON.parse(result) : result.toString(),
+        ),
+      );
   }
   public saveKey() {
-    this.clientService.saveKey(this.key).subscribe(() => { });
-  }
-  public notificationIdentifier(_index: number, item: NotificationModel) {
-    return item.dataUrl;
+    this.clientService.saveKey(this.key).subscribe(() => {});
   }
   public clearNotifications() {
-    this.clientService.clearNotifications().subscribe(() => { });
+    this.clientService.clearNotifications().subscribe(() => {});
   }
   private IsJson(value: string): boolean {
     try {
@@ -78,13 +93,20 @@ export class HomeComponent {
     this.showDialog.set(false);
   }
 
-  public handleDialogSubmit(event: { processedStatus: ProcessedStatus, comments: string }, notification: NotificationModel): void {
-    this.clientService.updateStatus(this.applicationName()[0], this.fileIdFromNotification()[0], this.version()[0], notification, event.processedStatus, event.comments, this.key, this.askJson);
+  public handleDialogSubmit(
+    event: { processedStatus: ProcessedStatus; comments: string },
+    notification: NotificationModel,
+  ): void {
+    this.clientService.updateStatus(
+      this.applicationName()[0],
+      this.fileIdFromNotification()[0],
+      this.version()[0],
+      notification,
+      event.processedStatus,
+      event.comments,
+      this.key,
+      this.askJson,
+    );
     this.closeNotificationDialog();
   }
-
 }
-
-
-
-
